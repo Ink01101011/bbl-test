0bea4d55f1b05307c90c3dbff23ad85dc7d000eb
Supakit <supakit.kitj@gmail.com>
2025-10-22 19:49:55 +0700

    initial app

---
+++ bbl-backend/src/main/java/com/th/bbl/backend/BblBackendApplication.java
@@ -0,0 +1,13 @@
+package com.th.bbl.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BblBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BblBackendApplication.class, args);
+	}
+
+}
---
+++ bbl-backend/src/test/java/com/th/bbl/backend/BblBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th.bbl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BblBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
